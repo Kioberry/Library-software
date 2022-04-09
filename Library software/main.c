fdb5337f00d9ad820ecd3223cf7e16c0a27f8b96
Takioberry <1046880511@qq.com>
2022-04-09 18:19:58 +0800

    I've add functions of borrow a book and return a book in the user system, I've also changed the user structure. What's more, I move all the menus to the main.c, which will be easier to write CLI and saves some hearder files. I have sort out them.

--- Library software/main.c
+++ Library software/main.c
@@ -1,14 +1,10 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-// Name:Wanrong Xie
-// Username:201487690
-// Date started:2022.3.15
-
 #include "book_management.h"
 #include "user.h"
 #include "conio.h"
-void menu() {
-	printf("\t(Logged in as: librarian)\n");
+
+//display the librarian menu
+void libmenu() {
+	printf("\n(Logged in as: librarian)\n");
 	printf("\n");
 	printf("---------------------------------------------------\n");
 	printf("\t1) Add a book\n");
@@ -17,13 +13,61 @@ void menu() {
 	printf("\t4) Display all books\n");
 	printf("\t5) Log out\n");
 	printf("\n");
-	printf("\tPlease choose an option, press Enter to confirm\n");
+	printf("Please choose an option, press Enter to confirm\n");
 	printf("---------------------------------------------------\n");
 	printf("\n");
 	printf("Option:");
 }
+void mainmenu() {
+	printf("\nWelcome to the Library Management System\n");
+	printf("\n");
+	printf("---------------------------------------------------\n");
+	printf("\t1) Register an account\n");
+	printf("\t2) Login\n");
+	printf("\t3) Search for books\n");
+	printf("\t4) Display all books\n");
+	printf("\t6) Quit\n");
+	printf("\n");
+	printf("Please choose an option, press Enter to confirm\n");
+	printf("---------------------------------------------------\n");
+	printf("\n");
+	printf("Option: ");
+}
+void usermenu() {
+	printf("\n");
+	printf("---------------------------------------------------\n");
+	printf("\t1) Borrow a book\n");
+	printf("\t2) Return a book\n");
+	printf("\t3) Display all books\n");
+	printf("\t4) Logged out\n\n");
+	printf("Please choose an option, press Enter to confirm\n");
+	printf("---------------------------------------------------\n");
+	printf("\n");
+	printf("Option:");
+}
+//The search interface shown on the screen
+void searchMenu() {
+	printf("\n");
+	printf("\n");
+	printf("---------------------------------------------------\n");
+	printf("\t1)Find books by title\n");
+	printf("\t2)Find books by authors\n");
+	printf("\t3)Find books by year\n");
+	printf("\t4)Return to previous menu\n");
+	printf("\nPlease choose an option, press Enter to confirm\n");
+	printf("---------------------------------------------------\n");
+	printf("\n");
+	printf("Option:");
+}
+
+//showing the clues for librarian to add a book and perform the add_book() function
 void add() {
+	int t1, t2, t3, t4, t5 = 0;
 	Book node;
+	node.id = 0;
+	node.copies = 0;
+	node.year = 0;
+	node.originc = 0;
 	node.title = (char*)calloc(1, 50 * sizeof(char));
 	node.authors = (char*)calloc(1, 50 * sizeof(char));
 	//unsigned int id; //Book ID
@@ -33,41 +77,73 @@ void add() {
 	//unsigned int copies; //number of copies the library has
 	//unsigned int originc;//number of the original copies the library has
 	printf("id:");
-	scanf("%d", &(node.id));
+	t1 = scanf("%d", &(node.id));
 	printf("title:");
-	scanf("%s", node.title);
+	t2 = scanf("%s", node.title);
 	printf("authors:");
-	scanf("%s", node.authors);
+	t3 = scanf("%s", node.authors);
 	printf("year:");
-	scanf("%d", &(node.year));
+	t4 = scanf("%d", &(node.year));
 	printf("copies:");
-	scanf("%d", &(node.copies));
-	printf("originc:");
-	scanf("%d", &(node.originc));
+	t5 = scanf("%d", &(node.copies));
+	if (t1 == 0 || t2 == 0 || t3 == 0 || t4 == 0 || t5 == 0) {
+		printf("Sorry, you attempt to add an invalid book, please try again.");
+		return;
+	}
+	if (node.year > 2022 || node.year < 0 || node.copies < 0) {
+		printf("Sorry, you attempt to add an invalid book, please try again.");
+		return;
+	}
+	node.originc = node.copies;
 	node.next = 0;
 	add_book(node);
 }
+
+//showing the clues for librarian to delete a book and perform the remove_book() function
 void del() {
 	int id;
-	printf("id:");
+	printf("Enter the id of the book you want to remove:");
 	scanf("%d", &id);
 	Book node;
 	node.id = id;
 	remove_book(node);
 }
+
+void bor() {
+	int id = 0;
+	printf("ID:");
+	scanf("%d", &id);
+	getchar();
+	bor_book(id);
+}
+
+void rtrn() {
+	int id = 0;
+	Book* node = (Book*)calloc(1, sizeof(Book));
+	node = theUser.bolist->next;
+	printf("ID\ttitle\tauthers\tyear\n");
+	printf("---------------------------------------------------\n");
+	while (node) {
+		printf("%d\t%s\t%s\t%d\n", node->id, node->title, node->authors, node->year);
+		node = node->next;
+	}
+
+	printf("Enter the id of the book you want to return: ");
+	scanf("%d", &id);
+	rtrn_book(id);
+}
+//display all the books in the library
 void display() {
 	Book* p = bhead->next;
+	printf("ID\ttitle\tauthors\tyear\tcopies\n");
+	printf("---------------------------------------------------\n");
 	while (p) {
-		printf("id:%d\n", p->id);
-		printf("title:%s\n", p->title);
-		printf("authors:%s\n", p->authors);
-		printf("year:%d\n", p->year);
-		printf("copies:%d\n", p->copies);
-		printf("orignc:%d\n", p->originc);
-		printf("\n");
+		printf("%d\t%s\t%s\t%d\t%d\n", p->id, p->title, p->authors, p->year, p->copies);
 		p = p->next;
 	}
 }
+
+
 void displaylist(BookList a) {
 	//unsigned int id; //Book ID
 //char* title; //book title
@@ -76,49 +152,64 @@ void displaylist(BookList a) {
 //unsigned int copies; //number of copies the library has
 //unsigned int originc;//number of the original copies the library has
 	Book* p = a.list;
-	for (int i0 = 0; i0 < a.length; i0++) {
+	int i0 = 0;
+	for (i0 = 0; i0 < a.length; i0++) {
 		/////////////////////////////////
 		printf("id:%d\n", p->id);
 		printf("title:%s\n", p->title);
 		printf("authors:%s\n", p->authors);
 		printf("year:%d\n", p->year);
 		printf("copies:%d\n", p->copies);
-		printf("orignc:%d\n", p->originc);
 		printf("\n");
 		p = p->next;
 	}
 }
+//The interface of search for books
 void find() {
-	printf("1.title\n");
-	printf("2.name\n");
-	printf("3.year\n");
-	printf("option:");
+	searchMenu();
 	int key = 0;
 	scanf("%d", &key);
 	BookList a;
-	if (key == 1) {
+	switch (key) {
+	case 1: {
 		char str[40];
+		printf("Enter the title of the book you want to search: ");
 		scanf("%s", str);
 		a = find_book_by_title(str);
+		if (a.list != NULL) {
+			displaylist(a);
+		}
+		break;
 	}
-	if (key == 2) {
-		char str[40];
-		scanf("%s", str);
-		a = find_book_by_author(str);
+	case 2: {
+		char str1[40];
+		printf("Enter the authors of the book(separate with commas): ");
+		scanf("%s", str1);
+		a = find_book_by_author(str1);
+		if (a.list != NULL) {
+			displaylist(a);
+		}
+		break;
 	}
-	if (key == 3) {
+	case 3: {
 		int year;
-		scanf("%s", &year);
+		printf("Enter the year of the book you want to search: ");
+		scanf("%d", &year);
 		a = find_book_by_year(year);
+		if (a.list != NULL) {
+			displaylist(a);
+		}
+		break;
 	}
-	displaylist(a);
+	default:
+		printf("error");
+	}
+
+
 }
-int main() {
-	user* userhead = (user*)calloc(1, sizeof(user));
-	bhead = (Book*)calloc(1, sizeof(Book));
-	load_user(userhead);
-	int tag = 1;
-	while (tag) {
+void reg() {
+	int tag = 2;
+	while (tag == 2) {
 		char name[50];
 		char password[50];
 		printf("intput name:");
@@ -129,7 +220,7 @@ int main() {
 		}
 		printf("input password:");
 		scanf("%s", password);
-		user* p = userhead->next;
+		user* p = uhead->next;
 		while (p) {
 			if (strcmp(p->name, name) == 0 && strcmp(p->password, password) == 0) {
 				tag = 1;
@@ -138,17 +229,102 @@ int main() {
 			p = p->next;
 		}
 
-		if (tag == 0) {
-			printf("try again\n");
+		if (tag == 2) {
+			printf("You have registered into the system successfully!\n");
+			user rUser;
+			strcpy(rUser.name, name);
+			strcpy(rUser.password, password);
+			rUser.bolist = (Book*)calloc(1, sizeof(Book));
+			add_user(rUser);
+			return;
 		}
 	}
+	if (tag == 1) {
+		printf("Sorry, registration unsuccessful, the username you entered already exist\n");
+		return;
+	}
+
+}
+void log() {
+	int tag = 1;
+	char name[50];
+	char password[50];
+	theUser.bolist = (Book*)calloc(1, sizeof(Book));
+	while (tag == 1) {
+		printf("intput name:");
+		scanf("%s", name);
+		printf("input password:");
+		scanf("%s", password);
+		if (strcmp(name, "librarian") == 0 && strcmp(password, "librarian") == 0) {
+			tag = 3;
+			break;
+		}
+		user* p = uhead->next;
+		while (p) {
+			if (strcmp(p->name, name) == 0 && strcmp(p->password, password) == 0) {
+				tag = 2;
+				strcpy(p->name, theUser.name);
+				strcpy(p->password, theUser.password);
+				Book* b = p->bolist;
+				b->id = theUser.bolist->id;
+				break;
+			}
+			p = p->next;
+		}
+		if (tag == 1) {
+			printf("The username or password is not true, try again\n");
+			return;
+		}
+
+	}
+	if (tag == 3) {
+		while (1) {
+			libmenu();
+			int key = 0;
+			scanf("%d", &key);
+			getchar();
+			switch (key) {
+			case 1:add(); break;
+			case 2:del(); break;
+			case 3:find(); break;
+			case 4:display(); break;
+			case 5:mainCLI();
+			default:
+				printf("try again\n");
+			}
+			getch();
+		}
+	}
+	if (tag == 2) {
+		while (1) {
+			printf("\n(Logged in as: %s)\n", name);
+			usermenu();
+			int key = 0;
+			scanf("%d", &key);
+			getchar();
+			switch (key) {
+			case 1:bor(); break;
+			case 2:rtrn(); break;
+			case 3:find(); break;
+			case 4:display(); break;
+			case 5:exit(0);
+			default:
+				printf("try again\n");
+			}
+			getch();
+		}
+	}
+}
+
+mainCLI() {
 	while (1) {
-		menu();
+		mainmenu();
 		int key = 0;
 		scanf("%d", &key);
+		getchar();
 		switch (key) {
-		case 1:add(); break;
-		case 2:del(); break;
+		case 1:reg(); break;
+		case 2:log(); break;
 		case 3:find(); break;
 		case 4:display(); break;
 		case 5:exit(0);
@@ -157,5 +333,13 @@ int main() {
 		}
 		getch();
 	}
+}
+int main() {
+	uhead = (user*)calloc(1, sizeof(user));
+	bhead = (Book*)calloc(1, sizeof(Book));
+	load_user(uhead);
+	//FILE* bfile = NULL;
+	//load_books(bfile);
+	mainCLI();
 	return 0;
 }
--- Library software/user.c
+++ Library software/user.c
@@ -1,3 +1,4 @@
+#include "book_management.h"
 #include "user.h"
 void load_user(user* userhead) {
 	FILE* fp = fopen("user.txt", "rb");
@@ -37,4 +38,107 @@ void save_user(user* userhead) {
 		p = p->next;
 	}
 	fclose(fp);
+}
+void load_loans(user* loanhead) {
+	FILE* fp = fopen("loans.txt", "rb");
+	if (fp == NULL) return;
+	int id = 0;
+	char username[50];
+	while (1) {
+		int tag = fscanf(fp, "%d %s", &id, username);
+		if (tag == -1) break;
+		int i0 = 0;
+		for (i0 = 0; i0 < strlen(username); i0++) {
+			if (username[i0] == '_')username[i0] = ' ';
+		}
+		user* node = (user*)calloc(1, sizeof(user));
+		node->bolist->id = id;
+		strcpy(node->name, username);
+		node->next = loanhead->next;
+		loanhead->next = node;
+	}
+
+}
+int add_user(user theUser) {
+	user* node = (user*)calloc(1, sizeof(user));
+	*node = theUser;
+	node->next = NULL;
+	user* p = uhead;
+	while (p) {
+		if (p->next == NULL) {
+			p->next = node;
+			break;
+		}
+		p = p->next;
+	}
+	return 0;
+}
+int add_ubook(Book book) {
+	Book* node = (Book*)calloc(1, sizeof(Book));
+	*node = book;
+	node->next = NULL;
+	Book* p = theUser.bolist;
+	while (p) {
+		if (p->next == NULL) {
+			p->next = node;
+			break;
+		}
+		p = p->next;
+	}
+	return 0;
+}
+int bor_book(int id) {
+	Book* p = bhead->next;
+	//user* lhead = (user*)calloc(1, sizeof(user));
+	//load_loans(lhead);
+	//user* lp = lhead->next;
+	//while (lp) {
+	//	if (theUser.name == lp->name && theUser.bolist->id == lp->bolist->id) {
+	//		printf("You have already borrowed this book.");
+	//		return 1;
+	//	}
+	//	lp = lp->next;
+	//}
+	while (p) {
+		if (p->id == id && p->copies > 0) {
+			printf("You have borrowed the book successfully!");
+			Book book;
+			book.id = 0;
+			book.year = 0;
+			book.copies = 0;
+			book.originc = 0;
+			book.title = (char*)calloc(1, 50 * sizeof(char));
+			book.authors = (char*)calloc(1, 50 * sizeof(char));
+			book.id = p->id;
+			book.year = p->year;
+			strcpy(p->authors, book.authors);
+			strcpy(p->title, book.title);
+			add_ubook(book, theUser);
+			return 0;
+		}
+		else if (p->id == id) {
+			printf("This book is out of loan.");
+			return 1;
+		}
+		p = p->next;
+	}
+	printf("The book id you entered is invalid");
+	return -1;
+}
+int rtrn_book(int id) {
+	Book* p = theUser.bolist->next;
+	Book* pre = theUser.bolist;
+	while (p) {
+		if (p->id == id) {
+			pre->next = p->next;
+			free(p->title);
+			free(p->authors);
+			free(p);
+			return 0;
+		}
+		p = p->next;
+		pre = pre->next;
+	}
+	printf("No book id could be found to return");
+	return 1;
 }
